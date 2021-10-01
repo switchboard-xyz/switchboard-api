2f28f00637422259a2330b755374fc429e4ce3ca
mgild <mgild@users.noreply.github.com>
2021-10-01 14:53:47 -0400

    Automatic iteration of switchboard-api

--- lib/compiled.d.ts
+++ lib/compiled.d.ts
@@ -1897,6 +1897,102 @@ export namespace OracleJob {
         public toJSON(): { [k: string]: any };
     }
 
+    /** Properties of a RegexExtractTask. */
+    interface IRegexExtractTask {
+
+        /** RegexExtractTask pattern */
+        pattern?: (string|null);
+
+        /** RegexExtractTask groupNumber */
+        groupNumber?: (number|null);
+    }
+
+    /** Represents a RegexExtractTask. */
+    class RegexExtractTask implements IRegexExtractTask {
+
+        /**
+         * Constructs a new RegexExtractTask.
+         * @param [properties] Properties to set
+         */
+        constructor(properties?: OracleJob.IRegexExtractTask);
+
+        /** RegexExtractTask pattern. */
+        public pattern: string;
+
+        /** RegexExtractTask groupNumber. */
+        public groupNumber: number;
+
+        /**
+         * Creates a new RegexExtractTask instance using the specified properties.
+         * @param [properties] Properties to set
+         * @returns RegexExtractTask instance
+         */
+        public static create(properties?: OracleJob.IRegexExtractTask): OracleJob.RegexExtractTask;
+
+        /**
+         * Encodes the specified RegexExtractTask message. Does not implicitly {@link OracleJob.RegexExtractTask.verify|verify} messages.
+         * @param message RegexExtractTask message or plain object to encode
+         * @param [writer] Writer to encode to
+         * @returns Writer
+         */
+        public static encode(message: OracleJob.IRegexExtractTask, writer?: $protobuf.Writer): $protobuf.Writer;
+
+        /**
+         * Encodes the specified RegexExtractTask message, length delimited. Does not implicitly {@link OracleJob.RegexExtractTask.verify|verify} messages.
+         * @param message RegexExtractTask message or plain object to encode
+         * @param [writer] Writer to encode to
+         * @returns Writer
+         */
+        public static encodeDelimited(message: OracleJob.IRegexExtractTask, writer?: $protobuf.Writer): $protobuf.Writer;
+
+        /**
+         * Decodes a RegexExtractTask message from the specified reader or buffer.
+         * @param reader Reader or buffer to decode from
+         * @param [length] Message length if known beforehand
+         * @returns RegexExtractTask
+         * @throws {Error} If the payload is not a reader or valid buffer
+         * @throws {$protobuf.util.ProtocolError} If required fields are missing
+         */
+        public static decode(reader: ($protobuf.Reader|Uint8Array), length?: number): OracleJob.RegexExtractTask;
+
+        /**
+         * Decodes a RegexExtractTask message from the specified reader or buffer, length delimited.
+         * @param reader Reader or buffer to decode from
+         * @returns RegexExtractTask
+         * @throws {Error} If the payload is not a reader or valid buffer
+         * @throws {$protobuf.util.ProtocolError} If required fields are missing
+         */
+        public static decodeDelimited(reader: ($protobuf.Reader|Uint8Array)): OracleJob.RegexExtractTask;
+
+        /**
+         * Verifies a RegexExtractTask message.
+         * @param message Plain object to verify
+         * @returns `null` if valid, otherwise the reason why it is not
+         */
+        public static verify(message: { [k: string]: any }): (string|null);
+
+        /**
+         * Creates a RegexExtractTask message from a plain object. Also converts values to their respective internal types.
+         * @param object Plain object
+         * @returns RegexExtractTask
+         */
+        public static fromObject(object: { [k: string]: any }): OracleJob.RegexExtractTask;
+
+        /**
+         * Creates a plain object from a RegexExtractTask message. Also converts values to other types if specified.
+         * @param message RegexExtractTask
+         * @param [options] Conversion options
+         * @returns Plain object
+         */
+        public static toObject(message: OracleJob.RegexExtractTask, options?: $protobuf.IConversionOptions): { [k: string]: any };
+
+        /**
+         * Converts this RegexExtractTask to JSON.
+         * @returns JSON object
+         */
+        public toJSON(): { [k: string]: any };
+    }
+
     /** Properties of a Task. */
     interface ITask {
 
@@ -1935,6 +2031,9 @@ export namespace OracleJob {
 
         /** Task maxTask */
         maxTask?: (OracleJob.IMaxTask|null);
+
+        /** Task regexExtractTask */
+        regexExtractTask?: (OracleJob.IRegexExtractTask|null);
     }
 
     /** Represents a Task. */
@@ -1982,8 +2081,11 @@ export namespace OracleJob {
         /** Task maxTask. */
         public maxTask?: (OracleJob.IMaxTask|null);
 
+        /** Task regexExtractTask. */
+        public regexExtractTask?: (OracleJob.IRegexExtractTask|null);
+
         /** Task Task. */
-        public Task?: ("httpTask"|"jsonParseTask"|"medianTask"|"meanTask"|"websocketTask"|"divideTask"|"multiplyTask"|"lpTokenPriceTask"|"lpExchangeRateTask"|"conditionalTask"|"valueTask"|"maxTask");
+        public Task?: ("httpTask"|"jsonParseTask"|"medianTask"|"meanTask"|"websocketTask"|"divideTask"|"multiplyTask"|"lpTokenPriceTask"|"lpExchangeRateTask"|"conditionalTask"|"valueTask"|"maxTask"|"regexExtractTask");
 
         /**
          * Creates a new Task instance using the specified properties.
